8a684812830bc804974271e6882748a96710f497
KostadinovK <kostadinovk2001@gmail.com>
2018-03-09 23:20:32 +0200

    solved exercises

--- 04-Connect-to-Internet/exercises/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ 04-Connect-to-Internet/exercises/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -16,6 +16,7 @@
 package com.example.android.datafrominternet.utilities;
 
 import android.net.Uri;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,6 +41,7 @@ public class NetworkUtils {
      */
     final static String PARAM_SORT = "sort";
     final static String sortBy = "stars";
+    private static final String TAG = "NetworkUtils";
 
     /**
      * Builds the URL used to query GitHub.
